28f2db487c50368b82d44ebf17edf4f362261f22
Masafumim Horimoto on aso15gw1 <masafumi.horimoto@gmail.com>
2019-10-20 01:18:54 +0900

    installerを作る。rc.localを追加した

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# comet1
+# Comet1
+
+必要なモジュール
+
+* i2c-tools
+* python3-smbus
+* python3-serial
+* python3-pip
+* pip3 install --upgrade OPi.GPIO
--- shutdown/bye.sh
+++ shutdown/bye.sh
@@ -1,6 +1,6 @@
-#! /bin/bash
+#! /bin/sh
 #set -x
 
 /usr/local/bin/writelcd.py "System is" 1 i
 /usr/local/bin/writelcd.py "    shutdown" 2 n
-echo 0 > /sys/class/gpio/gpio110/value
+echo 0 > /sys/class/leds/orangepi\:green\:pwr/brightness
